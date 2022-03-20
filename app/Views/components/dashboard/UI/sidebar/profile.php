064d44480764a819a2a014006c00ae906fa4bd75
Baharsah <69349170+baharsah@users.noreply.github.com>
2022-03-21 00:38:12 +0700

    [dashboard] env

--- app/Views/components/dashboard/UI/sidebar/profile.php
+++ app/Views/components/dashboard/UI/sidebar/profile.php
@@ -3,6 +3,6 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Guest (<?=ENVIRONMENT?>)</a>
         </div>
       </div>
