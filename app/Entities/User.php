c4b451edcba8e2c398f8787ae07d3a9f6b76cc87
Baharsah <69349170+baharsah@users.noreply.github.com>
2022-03-21 11:15:42 +0700

    [Auth] Modify

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -6,14 +6,14 @@ use CodeIgniter\Entity\Entity;
 
 class User extends Entity
 {
-    protected $datamap = [];
+    protected $datamap = [
+        "name" => "full_name"
+    ];
     protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
     protected $casts   = [];
     protected $attributes = [
         "id"       => NULL,
         "name"     => NULL,
-        "username" => NULL,
-        'name'     => NULL,
         'password' => NULL
     ];
 }
