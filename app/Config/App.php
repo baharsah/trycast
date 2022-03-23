60e7f312041b5ad6c33abe247bae7a66668c3ba8
Baharsah <baharsah@outlook.co.id>
2022-03-23 09:29:23 +0700

    new boot

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = BASE_URL;
+    public $baseURL = 'http://localhost:8080/';
 
     /**
      * --------------------------------------------------------------------------
