12ab87397cbc73bfd622f2e3814269dcda7eaa6b
Baharsah <baharsah@outlook.co.id>
2022-04-04 16:29:06 +0700

    [Auth]

--- app/Controllers/AuthCtrl.php
+++ app/Controllers/AuthCtrl.php
@@ -11,23 +11,47 @@ class AuthCtrl extends ConsumerController
 
     //Hint :  Use $this for loading sessions and responses
 
-    private function __authcheck($payload){
-        
-    }
     public function login(){
-        return view("components/board/blankLogin");
+        try{ 
+            return view("components/board/blankLogin");
+            
+           }catch (\Error $e){
+        
+           }
 
     }
 
     public function register(){
+       try{ 
         return view("components/board/blankRegister");
-
+        
+       }catch (\Error $e){
+    
+       }
         
     }
     public function registerStrategic(){
 
+        //Register
+
+        if($this->request->getPost() !== NULL){
+            // Registering post variables
+            $username = $this->request->getPost("username");
+            $password = $this->request->getPost("password");
+            // Check where user is available
+
+            try {
+                
+        
+            }catch(\Error $e){
+
+                log_message('notice' , 'Terjadi masalah pada sisi klien. data {error}' , ["error" => $e]);
+
+            }
+        }
+
     }
     public function loginStrategic(){
-        
+
     }
 }
