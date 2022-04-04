94860035528395961a050c6b4e78000ce03663d9
Baharsah <baharsah@outlook.co.id>
2022-04-04 15:00:30 +0700

    [Auth] users Validation

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,7 +11,7 @@ class UserModel extends Model
     protected $primaryKey       = 'username';
     protected $useAutoIncrement = false;
     protected $insertID         = 0;
-    protected $returnType       = 'array';
+    protected $returnType       = 'object';
     protected $useSoftDeletes   = true;
     protected $protectFields    = true;
     protected $allowedFields    = ['name' , 'password'];
@@ -24,19 +24,35 @@ class UserModel extends Model
     protected $deletedField  = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [];
+    protected $validationRules      = [
+        'username'     => 'required|alpha_dash|min_length[4]|is_unique[users.username]',
+        'password'     => 'required|min_length[8]|alpha_numeric_punct',
+        'pass_confirm' => 'required_with[password]|matches[password]',
+    ];
     protected $validationMessages   = [];
     protected $skipValidation       = false;
     protected $cleanValidationRules = true;
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
+    protected $beforeInsert   = ['hashPassword'];
     protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
+    protected $beforeUpdate   = ['hashPassword'];
     protected $afterUpdate    = [];
     protected $beforeFind     = [];
     protected $afterFind      = [];
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
+
+    protected function hashPassword(array $data)
+{
+    if (! isset($data['data']['password'])) {
+        return $data;
+    }
+
+    $data['data']['password_hash'] = password_hash($data['data']['password'], PASSWORD_DEFAULT);
+    unset($data['data']['password']);
+
+    return $data;
+}
 }
